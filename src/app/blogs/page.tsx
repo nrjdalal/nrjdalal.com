dc33a3546aac9f37c76cc3089e7ed17bed82e13a
Neeraj Dalal <admin@nrjdalal.com>
2025-02-22 03:30:06 +0530

    chore: page placeholders

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <main className="flex min-h-[calc(100dvh-3.5rem)] items-center justify-center">
-      Home - Coming Soon
+      Blogs - Coming Soon
     </main>
   )
 }
