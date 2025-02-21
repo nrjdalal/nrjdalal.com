c7f30eb9c6e2cb5c95918bdb51ea411f78ec5c44
Neeraj Dalal <admin@nrjdalal.com>
2025-02-22 03:31:15 +0530

    fix: pages info

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <main className="flex min-h-[calc(100dvh-3.5rem)] items-center justify-center">
-      Projects - Coming Soon
+      Contact - Coming Soon
     </main>
   )
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <main className="flex min-h-[calc(100dvh-3.5rem)] items-center justify-center">
-      Contact - Coming Soon
+      Projects - Coming Soon
     </main>
   )
 }
