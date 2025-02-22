0713cbdb69a278de898f75c4679308d8ebd1c9ab
Neeraj Dalal <admin@nrjdalal.com>
2025-02-23 03:47:58 +0530

    feat: toggle v3

--- src/components/nui/theme-toggle.tsx
+++ src/components/nui/theme-toggle.tsx
@@ -1,16 +1,15 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
 import { RiContrast2Fill, RiMoonFill, RiSunFill } from "@remixicon/react"
 import { useTheme } from "next-themes"
-import { useId, useState } from "react"
+import React, { useState } from "react"
 
-export function ThemeToggle() {
-  const id = useId()
+export const ThemeToggle = () => {
   const { theme, setTheme } = useTheme()
   const [system, setSystem] = useState(false)
 
   const smartToggle = () => {
-    /* The smart toggle by @nrjdalal */
     if (theme === "system") {
       const prefersDarkScheme = window.matchMedia(
         "(prefers-color-scheme: dark)",
@@ -24,35 +23,20 @@ export function ThemeToggle() {
   }
 
   return (
-    <>
-      <input
-        id={id}
-        type="checkbox"
-        name="theme-toggle"
-        className="peer sr-only"
-        checked={system}
-        onChange={smartToggle}
-        aria-label="Toggle dark mode"
-      />
-      <label
-        htmlFor={id}
-        className="hover:bg-border/50 hover:text-foreground text-foreground lg:text-muted-foreground flex aspect-square h-full cursor-pointer items-center justify-center border-t lg:border-0"
-        aria-hidden="true"
-      >
-        {system ? (
-          <RiContrast2Fill size={20} aria-hidden="true" />
-        ) : (
-          <>
-            <RiSunFill className="dark:hidden" size={20} aria-hidden="true" />
-            <RiMoonFill
-              className="hidden dark:block"
-              size={20}
-              aria-hidden="true"
-            />
-          </>
-        )}
-        <span className="sr-only">Switch to light/dark version</span>
-      </label>
-    </>
+    <Button
+      variant="ghost"
+      className="aspect-square size-full rounded-none [&_svg]:size-5"
+      onClick={smartToggle}
+      aria-label="Switch between system/light/dark version"
+    >
+      {system ? (
+        <RiContrast2Fill aria-hidden="true" />
+      ) : (
+        <>
+          <RiSunFill className="dark:hidden" aria-hidden="true" />
+          <RiMoonFill className="hidden dark:block" aria-hidden="true" />
+        </>
+      )}
+    </Button>
   )
 }
