2559bb718166b05718e4b1c4f019a715c5e35361
Neeraj Dalal <admin@nrjdalal.com>
2025-02-23 07:02:34 +0530

    chore: placeholder

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,20 @@
+import { Button } from "@/components/ui/button"
+import { RiGithubFill } from "@remixicon/react"
+import Link from "next/link"
+
 export default function Page() {
   return (
-    <main className="flex min-h-[calc(100dvh-3.5rem)] items-center justify-center">
-      Contact - Coming Soon
+    <main className="flex min-h-[calc(100dvh-3.5rem)] flex-col items-center justify-center p-5">
+      <p>UI, Tools and much more / Coming Soon</p>
+      <Link
+        href="https://github.com/nrjdalal/nrjdalal.com"
+        target="_blank"
+        className="mt-4 flex gap-2"
+      >
+        <Button className="[&_svg]:size-5">
+          Bookmark <RiGithubFill />
+        </Button>
+      </Link>
     </main>
   )
 }
