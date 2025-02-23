16059bb1cb822ddf02d827eba6b3572b70b19363
Neeraj Dalal <admin@nrjdalal.com>
2025-02-24 01:57:49 +0530

    chore: seperate utils

--- scripts/build-registry.ts
+++ scripts/build-registry.ts
@@ -1,49 +1,26 @@
-import fs from "node:fs"
-import { glob } from "tinyglobby"
+import { getAliases } from "./utils/aliases"
+import { getFiles, normalizeAndFilter } from "./utils/files"
 
-const files = async ({
-  patterns = ["**", ".**"],
-  ignore = [] as string[],
-} = {}) => {
-  patterns = Array.isArray(patterns) ? patterns : [patterns]
-  ignore = Array.isArray(ignore) ? ignore : [ignore]
-
-  if (fs.existsSync(".gitignore")) {
-    const gitignorePatterns: string[] = (
-      await fs.promises.readFile(".gitignore", "utf8")
-    )
-      .split("\n")
-      .map((line) => line.trim())
-      .filter((line) => line && !line.startsWith("#"))
-      .map((line) => line.replace(/^\//, ""))
-    ignore = ignore.concat(gitignorePatterns)
-  }
-
-  return await glob(patterns, {
-    ignore: ignore.filter((ig) => !patterns.includes(ig)),
-  })
+let config: { files: string[]; directories: string[] } = {
+  files: [],
+  directories: ["@/components/nui"],
 }
 
-const aliases = async () => {
-  const isTypescript = (await files()).includes("tsconfig.json")
+const files = await getFiles()
+const aliases = await getAliases()
 
-  if (isTypescript) {
-    const tsconfig = await fs.promises.readFile("tsconfig.json", "utf8")
-    const { compilerOptions } = JSON.parse(tsconfig)
-    if (compilerOptions && compilerOptions.paths) {
-      return Object.entries(
-        compilerOptions.paths as Record<string, [string]>,
-      ).reduce(
-        (acc, [key, [value]]) => {
-          acc[key.replace("/*", "")] = value.replace("/*", "")
-          return acc
-        },
-        {} as Record<string, string>,
-      )
-    }
-  }
-
-  return {}
+config = {
+  files: normalizeAndFilter({
+    paths: config.files,
+    files,
+    aliases,
+  }),
+  directories: normalizeAndFilter({
+    paths: config.directories,
+    files,
+    aliases,
+    directory: true,
+  }),
 }
 
-console.log(await aliases())
+console.log(config)
