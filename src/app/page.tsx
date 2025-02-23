a532be8f00895e09b3c5cd717f3b331ced9598e3
Neeraj Dalal <admin@nrjdalal.com>
2025-02-23 07:01:21 +0530

    test: prepend extraline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 
 export default function Page() {
   return (
-    <main className="flex min-h-[calc(100dvh-3.5rem)] flex-col items-center justify-center">
+    <main className="flex min-h-[calc(100dvh-3.5rem)] flex-col items-center justify-center p-5">
       <p>UI, Tools and much more / Coming Soon</p>
       <Link
         href="https://github.com/nrjdalal/nrjdalal.com"
